d73aa7e340b35a84bfd4f4105d663d15a1e737c8
Hao Wu <hwu@intellisurvey.com>
2016-07-17 13:04:23 -0700

    cython version

--- py/distance_one_cython.py
+++ py/distance_one_cython.py
@@ -11,8 +11,8 @@ with open(pickle_filepath, 'rb') as pickle_handle:
 
 def select_item(item, csf_cdr3 ):
     F = item.split(',');
-    pb_cdr3 = F[3];
-    return distance.hamming_distance( csf_cdr3, pb_cdr3, 2);
+    pb_cdr3 = F[3]
+    return distance.hamming_distance( csf_cdr3, pb_cdr3, 2)
 
 def process_file(csf_cdr3):
     selected = [ i for i in lines if select_item(i,csf_cdr3) == 1]
@@ -22,11 +22,11 @@ def process_file(csf_cdr3):
             f.write(str(s))
 
 csf_cdr3_lines = []
-with open('pb_cdr3.csv', 'r') as f:
+with open('csf_cdr3.csv', 'r') as f:
     csf_cdr3_lines = f.readlines()
 
 csf_cdr3 = [ i.split(',')[3] for i in csf_cdr3_lines]
 
 with Pool(6) as p:
-    p.map(process_file, csf_cdr3[0:100])
+    p.map(process_file, csf_cdr3)
 
